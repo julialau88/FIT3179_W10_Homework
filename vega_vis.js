3c3ac96a82b422ca830a52109084d275c643e418
julialau88 <62095412+julialau88@users.noreply.github.com>
2021-10-09 23:14:23 +0800

    Add files via upload
    Upload homework files

---
+++ vega_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "diabetesChangeFrom2010.vg.json";
+vegaEmbed("#change", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_1 = "diabetesGlobal.vg.json";
+vegaEmbed("#prevelance", vg_1).then(function(result) {
+}).catch(console.error);
